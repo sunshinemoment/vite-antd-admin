1ee0b0d17ca8f324791eb2f7c20c29ed91ba240f
biansaijun <875848407@qq.com>
2024-04-15 15:23:11 +0800

    chore: 升级ant 4

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
-import './antd.less';
 import './global.scss';
 
 const app = createApp(App);
